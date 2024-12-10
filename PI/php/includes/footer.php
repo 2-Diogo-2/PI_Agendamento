8ace048c430621bc2509deeb48aa1385e2d9a6fd
Diogo <161363747+2-Diogo-2@users.noreply.github.com>
2024-12-10 08:50:21 -0300

    Delete PI directory

--- PI/php/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-        <p>Plataforma de Agendamento de Shows.</p>
-        
-        <script src="js/notificacoes.js"></script> <!-- Exemplo de script para gerenciar notificações -->
-    </footer>
-</body>
-</html>
